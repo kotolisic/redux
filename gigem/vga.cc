cbeeedd812f374e39fe8b04b17e7f978c69c5357
kotolisic <kotolisic@gmail.com>
2020-02-18 13:00:05 +0300

    Чуток поправил, чтоб не забыть

--- gigem/vga.cc
+++ gigem/vga.cc
@@ -22,23 +22,24 @@ void Gigatron::vga_init() {
     row = 0;
     col = 0;
     
-    minRow = 27 + 8;
-    maxRow = minRow + 480;
+    minRow = 27 + 8;        // v.backPorch + v.pulse
+    maxRow = minRow + 480;  // v.visible
 
-    minCol = 48 + 96;
-    maxCol = minCol + 640;
+    minCol = 48 + 96;       // h.backPorch + h.pulse
+    maxCol = minCol + 640;  // h.visible
 
     vga_out = 0;
 }
 
+// Обработчик VGA эмулятора
 void Gigatron::vga_tick() {
 
-    uint8_t let_out = out; // cpu.out
-    uint8_t falling = vga_out & ~let_out; // vga.out 
+    uint8_t let_out = out;                  // cpu.out
+    uint8_t falling = vga_out & ~let_out;   // vga.out 
 
-    // After 4 more CPU cycles HSYNC increments row to 0
+    // После 4 и более CPU циклов HSYNC увеличит row до 0
     if (falling & VSYNC) {
-        row   = -1; 
+        row = -1; 
     }
 
     if (falling & HSYNC) {
@@ -58,7 +59,6 @@ void Gigatron::vga_tick() {
         for (int i = 0; i < 4; i++) { 
             pset((col - minCol) + i, (row - minRow), (85*let_r)*65536 + (85*let_g)*256 + (85*let_b));
         }
-
     }
 
     col += 4;
